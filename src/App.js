7a015b5229731c5f164081feb2623ec1ca7f98ed
sajjad-jbr <job.sajjad.jbr@gmail.com>
2023-01-09 10:42:18 +0330

    fix layout and semantic tag

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React from 'react';
 import './App.css';
+import Header from "./Components/Header";
+import Main from "./Components/Main";
+import Footer from "./Components/Footer";
 
 function App() {
     return (
-        <div className="App">
-            hello world little lemon
-        </div>
+        <>
+            <Header/>
+            <Main/>
+            <Footer/>
+        </>
     );
 }
 
