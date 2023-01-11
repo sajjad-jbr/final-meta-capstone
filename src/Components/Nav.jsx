43946da547fb0c28cbb0fe8efe00567859fabc53
sajjad-jbr <job.sajjad.jbr@gmail.com>
2023-01-11 10:11:49 +0330

    add styles and fix responsive

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import icon_ham from '../Assets/images/svg/icon _hamburger menu.svg'
+import closeIcon from '../Assets/images/svg/x-symbol-svgrepo-com.svg'
+import ListMenu from "./ListMenu";
 
 const menus = [
     {id: 1, title: 'Home', link: 'home'},
@@ -10,18 +13,48 @@ const menus = [
 ]
 
 function Nav(props) {
+
+    useEffect(() => {
+        window.addEventListener('resize', (e) => {
+            if (e.currentTarget.innerWidth > 768) {
+                let navMobile = document.querySelector('#navbar-mobile-mode-container')
+                navMobile.classList.replace('show', 'hide')
+                if (statusMobileMenu !== true)
+                    setStatusMobileMenu(true)
+
+            }
+        })
+    }, []);
+
+
+    const [statusMobileMenu, setStatusMobileMenu] = useState(true);
+    const handleNavMobileMode = () => {
+        let navMobile = document.querySelector('#navbar-mobile-mode-container')
+        if (navMobile.classList.toString().includes('hide') || navMobile.classList.toString().length === 0) {
+            navMobile.classList.remove('hide')
+            navMobile.classList.add('show')
+            setStatusMobileMenu(false)
+        } else {
+            navMobile.classList.remove('show')
+            navMobile.classList.add('hide')
+            setStatusMobileMenu(true)
+        }
+    }
     return (
-        <nav className="little-lemon-navbar">
-            <ul className="list-menu">
-                {
-                    menus.map(item => <li className="list-item h-space-item d-inline" key={item.id}>
-                    <a className="link" href={item.link}>
-                        {item.title}
-                    </a>
-                </li>)
-                }
-            </ul>
-        </nav>
+        <>
+            <nav className="little-lemon-navbar">
+                <ListMenu className="h-space-item d-inline"/>
+            </nav>
+            <nav className="little-lemon-navbar-mobile-mode">
+                <button onClick={handleNavMobileMode}
+                        style={{background: 'transparent', border: 'none', outline: 'none'}}>
+                    <img src={statusMobileMenu ? icon_ham : closeIcon} alt="hamburger menu icon"
+                         style={{width: 20, height: 20}}/>
+                </button>
+            </nav>
+
+
+        </>
     );
 }
 
