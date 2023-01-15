6e5e617619f6b0bdd4b2eabedebd8eaee2a88e1c
sajjad-jbr <job.sajjad.jbr@gmail.com>
2023-01-15 14:48:15 +0330

    add new test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
-import { render, screen } from '@testing-library/react';
+/*import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
-});
+});*/
+
+import { render, screen } from "@testing-library/react";
+import FormBooking, {initializeTimes} from './Components/Booking/FormBooking';
+import {updateTimes} from "./util/api";
+
+test('Renders the BookingForm heading', () => {
+  render(<FormBooking />);
+  const headingElement = screen.getByText("Reservation Table");
+  expect(headingElement).toBeInTheDocument();
+
+})
+
+test('initializeTimes returns values', () => {
+  const times = initializeTimes()
+  expect(times).toBeTruthy()
+})
+
+test('updateTimes returns values', () => {
+  const newTimes = updateTimes('test value', 'action')
+  expect(newTimes).toEqual('test value')
+})
