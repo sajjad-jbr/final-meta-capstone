8b68662fd8a40144da5a5c2076d30d20725d42f7
ui11 <ui11@asd.comk>
2023-01-17 10:04:55 +0330

    fix ReservationsPage

--- src/Pages/ReservationsPage.jsx
+++ src/Pages/ReservationsPage.jsx
@@ -4,14 +4,11 @@ import restaurant from '../Assets/images/jpg/restaurant.jpg'
 import {Flex} from '@chakra-ui/react'
 function ReservationsPage(props) {
 
-    const getAvailableTimes = (data) => {
-        console.log("sajjad: ", data)
-    }
     return (
         <Flex direction="column" align="center">
             <img src={restaurant} alt="restaurant" width="50%" height="300px" style={{borderRadius: 10}}/>
 
-            <FormBooking getAvailableTimes={getAvailableTimes}/>
+            <FormBooking />
         </Flex>
     );
 }
